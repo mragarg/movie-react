27d504a2115a5be33ce9f7ff5815f02a970e68c3
Ashish Garg <mragarg@gmail.com>
2019-04-25 13:21:38 -0400

    Adds GenreList and movie Object

--- src/App.js
+++ src/App.js
@@ -2,23 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import movies from './movies';
+import GenreList from './GenreList';
+
 function App() {
   return (
+    console.log(movies);
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GenreList genres={}/>
     </div>
   );
 }
