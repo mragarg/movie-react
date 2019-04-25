97f8e5d0716bef2370cf0d2b2206f10852de70d8
Ashish Garg <mragarg@gmail.com>
2019-04-25 13:24:02 -0400

    Reformatted files, fixed exports and console.logs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import movies from './movies';
+
 import GenreList from './GenreList';
 
+
 function App() {
+  console.log(Object.keys(movies));
   return (
-    console.log(movies);
     <div className="App">
-      <GenreList genres={}/>
+      {/* <GenreList genres={}/> */}
     </div>
   );
 }
--- src/GenreList.js
+++ src/GenreList.js
@@ -3,9 +3,10 @@ import React from 'react';
 function GenreList(props) {
     return (
         <ul>
-            {/* list out those genres */}
+            {/* list out those genres! */}
+
         </ul>
-    )
+    );
 }
 
 export default GenreList;
